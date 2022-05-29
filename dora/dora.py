21ecedd5971913c11af3fc195ff1c9a59fca402e
mayukhdeb <mayukhmainak2000@gmail.com>
2022-05-29 14:02:44 +0530

    add docstring :pencil:

--- dora/dora.py
+++ dora/dora.py
@@ -17,6 +17,18 @@ class Dora():
         delete_if_storage_dir_exists = False,
         device = None
     ):
+        """Handles all stuff dora related. Would require a storage_dir where it would store the synthetic activatiion
+        maximization signals (s-AMS) as images which would be fed into self.model to collect activations.
+
+        Note: It is highly recommended that you run DORA on a GPU and not on a CPU for fast performance.
+
+        Args:
+            model (nn.Module): the pytorch model you'd want to work on
+            layer (nn.Module): model.some_layer
+            storage_dir (str, optional): name of the folder where the s-AMS images would be kept. Defaults to '.dora/'.
+            delete_if_storage_dir_exists (bool, optional): if True, will delete all the stuff that exists in storage_dir before using it. Defaults to False.
+            device (str, optional): specifies the device to be used for the model, for example, 'cuda:0'. If set to None, it automatically looks for a device. Defaults to None.
+        """
 
         if device is None:
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")

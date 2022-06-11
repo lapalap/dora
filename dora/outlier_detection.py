fdfd0a0938d03639b29d6df4e5cf40b8ff800e22
mayukhdeb <mayukhmainak2000@gmail.com>
2022-06-11 17:12:24 +0530

    add outlier detection boilerplate

--- dora/outlier_detection.py
+++ dora/outlier_detection.py
@@ -1,23 +1,43 @@
 import umap
+import plotly.express as px
+from pyod.models.pca import PCA
+
+OD_METHODS = {
+    "PCA": PCA
+    ## add more later
+}
 
 
-class UmapVisualizer:
-    def __init__(self):
+class OutlierDetector:
+    def __init__(self, name="PCA"):
 
+        # self.od =
         self.reducer = umap.UMAP()
         self.embeddings = None
+        self.activations = None
 
     def run(self, activations):
-        num_items = activations.shape[0]
+        self.activations = activations
         self.embeddings = self.reducer.fit_transform(activations)
+
+        outliers = self.detect_outliers(activations=activations)
+        print("DONE")
+
+    def detect_outliers(self, activations):
+        raise NotImplementedError
 
     def save(self, filename):
         assert (
             self.embeddings is not None
-        ), "Embeddings are still done, maybe you did not run UMAP yet"
+        ), "Embeddings are still None, maybe you did not run UMAP yet"
         raise NotImplementedError
 
     def show(self):
         assert (
             self.embeddings is not None
-        ), "Embeddings are still done, maybe you did not run UMAP yet"
+        ), "Embeddings are still None, maybe you did not run UMAP yet"
+
+        fig = px.scatter(
+            x=self.embeddings[:, 0], y=self.embeddings[:, 1], title="DORA Results"
+        )
+        fig.show()
